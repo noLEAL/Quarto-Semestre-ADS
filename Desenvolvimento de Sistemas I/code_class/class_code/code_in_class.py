e542773d028a0c83711196ad23b9b07acd038760
noLEAL <brunopazet@gmail.com>
2024-02-26 21:26:01 -0300

    .

---
+++ Desenvolvimento de Sistemas I/code_class/class_code/code_in_class.py
@@ -0,0 +1,21 @@
+nome = input("Digite seu nome: ")
+
+print(f"Seu nome em {nome}")
+
+nr1 = input("Digite o primeiro número:")
+nr2 = input("Digite o segundo número:")
+soma = nr1 + nr2
+
+print(f"A soma:{soma}")
+
+inr1 = int(nr1)
+inr2 = int(nr2)
+soma = inr1 + inr2
+
+print(f"A soma:{soma}")
+
+fnr1 = float(nr1)
+fnr2 = float(nr2)
+soma = fnr1+fnr2
+
+print(f"A soma:{soma}")
